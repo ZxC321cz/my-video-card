163cc13dedfed530aa6ea6e3edeaf75d14932961
ZxC321cz <yandex.cruche@gmail.com>
2026-04-11 00:33:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,41 @@ const overlay = document.querySelector(".overlay");
 const videoLayer = document.getElementById("video-layer");
 
 const videoUrl =
-    "https://www.youtube.com/embed/1gfDZ6YojGk?autoplay=1&mute=1&controls=1&loop=1&playlist=1gfDZ6YojGk&rel=0&modestbranding=1";
+    "https://www.youtube.com/embed/1gfDZ6YojGk?autoplay=1&mute=1&controls=1&loop=1&playlist=1gfDZ6YojGk&rel=0&modestbranding=1&enablejsapi=1&playsinline=1";
+
+function sendPlayerCommand(func, args = []) {
+    if (!iframe.contentWindow) {
+        return;
+    }
+
+    iframe.contentWindow.postMessage(
+        JSON.stringify({
+            event: "command",
+            func,
+            args
+        }),
+        "*"
+    );
+}
+
+function setStartVolume() {
+    sendPlayerCommand("setVolume", [20]);
+    sendPlayerCommand("unMute");
+    sendPlayerCommand("playVideo");
+}
 
 btn.addEventListener("click", () => {
     if (!iframe.getAttribute("src")) {
         iframe.src = videoUrl;
+        iframe.addEventListener(
+            "load",
+            () => {
+                setTimeout(setStartVolume, 350);
+            },
+            { once: true }
+        );
+    } else {
+        setStartVolume();
     }
 
     content.classList.add("hidden");
@@ -18,5 +48,6 @@ btn.addEventListener("click", () => {
 
     setTimeout(() => {
         overlay.classList.add("is-hidden");
+        setStartVolume();
     }, 700);
 });
